f51e8407881f296dc94cabbe9ee5e25c7686db1f
Derek-PC\Derek <dricciuto@gmail.com>
2023-06-26 23:27:21 -0700

    Added GET -> String x2 job to testnets

---
+++ docs/services/jobs/testnets/Binance-Testnet/get_string_x2/get_string_x2.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+contract getString is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    string public str1;
+    string public str2;
+
+    bytes32 private jobId;
+    uint256 private fee;
+
+    event requestStringFulfilled(bytes32 indexed requestId, string str1, str2);
+
+/// [constructor]    
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06);
+        setChainlinkOracle(0xd08FEb8203E76f836D74608595346ab6b0f768C9);
+        jobId = "be46cb8070c14cb7ad058b29f127ec6b";
+        fee = (0 * LINK_DIVISIBILITY) / 10; // 0 LINK (varies by network and job)
+    }
+/// [constructor]
+
+/// [request]
+    function requestStringData() public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillString.selector
+        );
+        
+        req.add(
+            "url1", 
+            "API_URL" // Example: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10
+        );
+        req.add("path1", "JSON_PATH"); // Example: 0,id
+        req.add(
+            "url2", 
+            "API_URL" // Example: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10
+        );
+        req.add("path2", "JSON_PATH"); // Example: 0,id
+        
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+/// [request]
+
+    function fulfillString(
+        bytes32 _requestId,
+        string memory _str1,
+        string memory _str2
+    ) public recordChainlinkFulfillment(_requestId) {
+        emit requestStringFulfilled(_requestId, _str1, _str2);
+        str1 = _str1;
+        str2 = _str2;
+    }
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
