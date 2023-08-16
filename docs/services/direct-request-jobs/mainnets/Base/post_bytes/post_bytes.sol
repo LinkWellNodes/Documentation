7e0220949745b6fedcf5951ab9320748c4b52a1f
Ken <mav3rick705@gmail.com>
2023-08-16 07:44:33 -0400

    Added jobs/contracts support and updated website docs to display CBM/OM jobs.

---
+++ docs/services/direct-request-jobs/mainnets/Base/post_bytes/post_bytes.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * THIS EXAMPLE USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+contract postBytes is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    // variable bytes(arbitrary-length raw byte data) returned in a single oracle response
+    bytes public data;
+    bytes32 private jobId;
+    uint256 private fee;
+
+
+/// [constructor]    
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0xefEa85a7D80425F7641878089Ae976c00980987D);
+        setChainlinkOracle(0xc287d52DFF95A6A49bdd2c3BB985c0E581b33d9c);
+        jobId = "b3390c03bfc24b42a0b0ab8051471bbb";
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0.1 LINK (varies by network and job)
+    }
+/// [constructor]    
+
+/// [request]
+    function postBytes() public {
+        Chainlink.Request memory req = buildOperatorRequest(
+            jobId,
+            this.fulfillBytes.selector
+        );
+        
+        // THE URL TO WHICH TO SEND THIS REQUEST
+        req.add("post", "API_URL"); // Example: https://myUrl.com
+        
+        // REQUEST BODY | cannot be an empty string ("{}" is OK)
+		req.add("requestData", "{}"); // Example: '{"fsyms": "LINK", "tsyms": "USD"}'
+        req.add("path", "JSON_PATH"); // Example: "RAW,LINK,USD,LASTMARKET"
+        sendOperatorRequest(req, fee);
+    }
+/// [request]
+
+    event RequestFulfilled(bytes32 indexed requestId, bytes indexed data);
+
+    function fulfillBytes(
+        bytes32 requestId,
+        bytes memory bytesData
+    ) public recordChainlinkFulfillment(requestId) {
+        emit RequestFulfilled(requestId, bytesData);
+        data = string(bytesData);
+
+    }
+
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
