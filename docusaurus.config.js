90e5a498bb9cf28a5d2c4a19502dff6c44bdad76
Ken <mav3rick705@gmail.com>
2023-09-01 13:18:14 -0400

    Modified docusaurus config file, removed v1 jobs due to broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs',
+                to: '/',
               },
               {
                 label: 'Services',
