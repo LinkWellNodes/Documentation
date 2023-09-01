b2b46f26b74521b9e4df9c273a043d12bbbe0dc1
Ken <mav3rick705@gmail.com>
2023-09-01 10:44:10 -0400

    minor config changes, added CNAME file.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://LinkWellNodes.io',
+  url: 'https://linkwellnodes.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'LinkWell Nodes', // Usually your GitHub org/user name.
+  organizationName: 'LinkWellNodes', // Usually your GitHub org/user name.
   projectName: 'Documentation', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
@@ -46,13 +46,14 @@ const config = {
           routeBasePath: '/',  // this should be the word that you want to set
           sidebarPath: require.resolve('./sidebars.js'),
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   // editUrl:
+        //   //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
         theme: {
           customCss: require.resolve('./src/css/style.css'),
         },
