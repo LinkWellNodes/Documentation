9a2e8bc202739403969641d92726ec1f27ad8e9e
Derek-PC\Derek <dricciuto@gmail.com>
2023-11-12 22:46:10 -0500

    Move favicon file to fix SEO issues

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'LinkWell Nodes',
   tagline: 'Chainlink Node Operations Team',
-  favicon: 'img/favicon.ico',
+  favicon: 'favicon.ico',
 
   // Set the production url of your site here
   url: 'https://docs.linkwellnodes.io',
