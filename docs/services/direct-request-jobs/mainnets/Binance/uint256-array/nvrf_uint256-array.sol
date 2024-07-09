7dd5ddefe71a2a0aaef5b6717989875036173538
Derek-PC\Derek <dricciuto@gmail.com>
2024-07-08 22:24:36 -0700

    Added nVRF jobs

---
+++ docs/services/direct-request-jobs/mainnets/Binance/uint256-array/nvrf_uint256-array.sol
@@ -0,0 +1,85 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ */
+
+contract LinkWellNVRFUint256ArrConsumerContractExample is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+	address private oracleAddress;
+    bytes32 private jobId;
+    uint256 private fee;
+    
+    constructor() ConfirmedOwner(msg.sender) {
+        _setChainlinkToken(0x404460C6A5EdE2D891e8297795264fDe62ADBB75);
+        setOracleAddress(<oracle address>);
+        setJobId("1b9b1967e5084cb59c2f04bcfc5307cb");
+        setFeeInHundredthsOfLink(12);     // 0.12 LINK
+    }
+
+    // Send a request to the Chainlink oracle
+    function request() public {
+    
+        Chainlink.Request memory req = _buildOperatorRequest(jobId, this.fulfill.selector);
+		     
+        // DEFINE THE REQUEST PARAMETERS (example)
+        req._addInt('minVal', 0);
+        req._addInt('maxVal', 500);
+        req._addInt('quantity', 5);
+        req._add('contact', '');     // PLEASE ENTER YOUR CONTACT INFO. this allows us to notify you in the event of any emergencies related to your request (ie, bugs, downtime, etc.). example values: 'derek_linkwellnodes.io' (Discord handle) OR 'derek@linkwellnodes.io' OR '+1-617-545-4721'
+        
+        // Send the request to the Chainlink oracle
+        _sendOperatorRequest(req, fee);
+    }
+
+    uint256[] public randomValue;
+
+    // Receive the result from the Chainlink oracle
+    event RequestFulfilled(bytes32 indexed requestId);
+    function fulfill(bytes32 requestId, uint256[] memory _randomValue) public recordChainlinkFulfillment(requestId) {
+        // Process the oracle response
+        // emit RequestFulfilled(requestId);    // (optional) emits this event in the on-chain transaction logs, allowing Web3 applications to listen for this transaction
+        randomValue = _randomValue;
+    }
+
+    // Update oracle address
+    function setOracleAddress(address _oracleAddress) public onlyOwner {
+        oracleAddress = _oracleAddress;
+        _setChainlinkOracle(_oracleAddress);
+    }
+    function getOracleAddress() public view onlyOwner returns (address) {
+        return oracleAddress;
+    }
+
+    // Update jobId
+    function setJobId(string memory _jobId) public onlyOwner {
+        jobId = bytes32(bytes(_jobId));
+    }
+    function getJobId() public view onlyOwner returns (string memory) {
+        return string(abi.encodePacked(jobId));
+    }
+    
+    // Update fees
+    function setFeeInJuels(uint256 _feeInJuels) public onlyOwner {
+        fee = _feeInJuels;
+    }
+    function setFeeInHundredthsOfLink(uint256 _feeInHundredthsOfLink) public onlyOwner {
+        setFeeInJuels((_feeInHundredthsOfLink * LINK_DIVISIBILITY) / 100);
+    }
+    function getFeeInHundredthsOfLink() public view onlyOwner returns (uint256) {
+        return (fee * 100) / LINK_DIVISIBILITY;
+    }
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(_chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
---
+++ docs/services/direct-request-jobs/testnets/Binance-Testnet/uint256/nvrf_uint256.sol
@@ -0,0 +1,89 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ */
+
+contract LinkWellNVRFUint256ConsumerContractExample is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+	address private oracleAddress;
+    bytes32 private jobId;
+    uint256 private fee;
+    
+    constructor() ConfirmedOwner(msg.sender) {
+        _setChainlinkToken(0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06);
+        setOracleAddress(0xd08FEb8203E76f836D74608595346ab6b0f768C9);
+        setJobId("92b7c5a0307545d9ad032f00523605a0");
+        setFeeInHundredthsOfLink(0);     // 0 LINK
+    }
+
+    // Send a request to the Chainlink oracle
+    function request() public {
+    
+        Chainlink.Request memory req = _buildOperatorRequest(jobId, this.fulfill.selector);
+		     
+        // DEFINE THE REQUEST PARAMETERS (example)
+        req._addInt('minVal', 0);
+        req._addInt('maxVal', 500);
+        req._add('contact', '');     // PLEASE ENTER YOUR CONTACT INFO. this allows us to notify you in the event of any emergencies related to your request (ie, bugs, downtime, etc.). example values: 'derek_linkwellnodes.io' (Discord handle) OR 'derek@linkwellnodes.io' OR '+1-617-545-4721'
+        
+        // Send the request to the Chainlink oracle
+        _sendOperatorRequest(req, fee);
+    }
+
+    uint256 public randomValue;
+
+    // Receive the result from the Chainlink oracle
+    event RequestFulfilled(bytes32 indexed requestId);
+    function fulfill(bytes32 requestId, uint256 _randomValue) public recordChainlinkFulfillment(requestId) {
+        // Process the oracle response
+        // emit RequestFulfilled(requestId);    // (optional) emits this event in the on-chain transaction logs, allowing Web3 applications to listen for this transaction
+        randomValue = _randomValue;
+    }
+
+    // Update oracle address
+    function setOracleAddress(address _oracleAddress) public onlyOwner {
+        oracleAddress = _oracleAddress;
+        _setChainlinkOracle(_oracleAddress);
+    }
+    function getOracleAddress() public view onlyOwner returns (address) {
+        return oracleAddress;
+    }
+
+    // Update jobId
+    function setJobId(string memory _jobId) public onlyOwner {
+        jobId = bytes32(bytes(_jobId));
+    }
+    function getJobId() public view onlyOwner returns (string memory) {
+        return string(abi.encodePacked(jobId));
+    }
+    
+    // Update fees
+    function setFeeInJuels(uint256 _feeInJuels) public onlyOwner {
+        fee = _feeInJuels;
+    }
+    function setFeeInHundredthsOfLink(uint256 _feeInHundredthsOfLink) public onlyOwner {
+        setFeeInJuels((_feeInHundredthsOfLink * LINK_DIVISIBILITY) / 100);
+    }
+    function getFeeInHundredthsOfLink() public view onlyOwner returns (uint256) {
+        return (fee * 100) / LINK_DIVISIBILITY;
+    }
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(_chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
